56bc1a28071446abb9306d51e5a79e35bcaa584f
Lars Kristian Dahl <lars.dahl@gmail.com>
2017-03-14 09:30:55 +0100

    Implement screen boundary entity handling
    Add and implement BoundaryComponent
    BoundaryComponent holds the mode for how the entity interacts with the screen boundary
    Add and implement BoundarySystem
    Regular entities are destroyed, entities with MODE_FREE are mirrored to the opposite end of their bounded axis.

--- core/src/no/ntnu/tdt4240/asteroids/entity/util/ComponentMappers.java
+++ core/src/no/ntnu/tdt4240/asteroids/entity/util/ComponentMappers.java
@@ -2,6 +2,7 @@ package no.ntnu.tdt4240.asteroids.entity.util;
 
 import com.badlogic.ashley.core.ComponentMapper;
 
+import no.ntnu.tdt4240.asteroids.entity.component.BoundaryComponent;
 import no.ntnu.tdt4240.asteroids.entity.component.DrawableComponent;
 import no.ntnu.tdt4240.asteroids.entity.component.MovementComponent;
 import no.ntnu.tdt4240.asteroids.entity.component.PositionComponent;
@@ -11,4 +12,5 @@ public abstract class ComponentMappers {
     public static final ComponentMapper<PositionComponent> positionMapper = ComponentMapper.getFor(PositionComponent.class);
     public static final ComponentMapper<MovementComponent> movementMapper = ComponentMapper.getFor(MovementComponent.class);
     public static final ComponentMapper<DrawableComponent> drawableMapper = ComponentMapper.getFor(DrawableComponent.class);
+    public static final ComponentMapper<BoundaryComponent> boundaryMapper = ComponentMapper.getFor(BoundaryComponent.class);
 }
