0634988497bcc49e853d517c1a2e7cbd6b7d58a2
Sander Aker Christiansen <hanakmumakil23@gmail.com>
2017-03-17 11:24:12 +0100

    Change obstacle spawn and movement (#14)
    * Change obstacle spawn and movement
    
    * Change posistion of spawn to outside of screen correctly

--- core/src/no/ntnu/tdt4240/asteroids/entity/World.java
+++ core/src/no/ntnu/tdt4240/asteroids/entity/World.java
@@ -8,6 +8,7 @@ import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.ashley.utils.ImmutableArray;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 
 import java.util.Random;
 
@@ -102,16 +103,43 @@ public class World {
     private Entity createObstacle() {
         Entity obstacle = EntityFactory.getInstance().createObstacle();
 
-        // TODO: Improve obstacle spawn position
         PositionComponent position = obstacle.getComponent(PositionComponent.class);
         DrawableComponent drawable = obstacle.getComponent(DrawableComponent.class);
 
-        int x = MathUtils.random(0, Gdx.graphics.getWidth());
-        int y = Gdx.graphics.getHeight() + drawable.region.getRegionHeight() / 2;
-        position.position.set(x, y);
+        int obstacleSide = MathUtils.random(4);
+        // 0 = top-spawn, 1 = bottom-spawn, 2 = left-spawn, 3 = right-spawn
+        int x, y;
+        float xVec, yVec;
+        int halfRegionHeight = drawable.region.getRegionHeight()/2;
+        int halfRegionWidth = drawable.region.getRegionWidth()/2;
+        int graphicsWidth = Gdx.graphics.getWidth();
+        int graphicsHeight = Gdx.graphics.getHeight();
 
+        // Based on spawn, position and movement (always inwards) is generated randomly.
+        if (obstacleSide < 2){
+            x = MathUtils.random(-halfRegionWidth, graphicsWidth + halfRegionWidth);
+            xVec = MathUtils.random(-100,101);
+            yVec = MathUtils.random()*200;
+            if (obstacleSide == 0){
+                y = -halfRegionHeight;
+            } else {
+                yVec *= -1;
+                y = graphicsHeight + halfRegionHeight;
+            }
+        } else {
+            y = MathUtils.random(-halfRegionHeight, graphicsHeight + halfRegionHeight);
+            yVec = MathUtils.random(-100, 101);
+            xVec = MathUtils.random()*200;
+            if (obstacleSide == 2){
+                x = -halfRegionWidth;
+            } else {
+                x = graphicsWidth + halfRegionWidth;
+                xVec *= -1;
+            }
+        }
+        position.position.set(x, y);
         MovementComponent movement = obstacle.getComponent(MovementComponent.class);
-        movement.velocity.setToRandomDirection().clamp(100, 250);
+        movement.velocity.set(xVec, yVec);
         return obstacle;
     }
 
