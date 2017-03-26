17b5d17b6d9683fc7e9a84bf8ee2563fb1df677e
morten2311 <morten23_lundenes@hotmail.com>
2017-03-26 12:59:18 +0200

    Added background sound,

--- core/src/no/ntnu/tdt4240/asteroids/Asteroids.java
+++ core/src/no/ntnu/tdt4240/asteroids/Asteroids.java
@@ -3,6 +3,7 @@ package no.ntnu.tdt4240.asteroids;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -17,7 +18,7 @@ public class Asteroids extends Game {
     public static final int VIRTUAL_HEIGHT = 1080;
     public static final int GUI_VIRTUAL_WIDTH = 1920/3;
     public static final int GUI_VIRTUAL_HEIGHT = 1080/3;
-
+    private Music music;
 
     Asteroids(INetworkService networkService) {
         Asteroids.networkService = networkService;
@@ -32,6 +33,10 @@ public class Asteroids extends Game {
         Gdx.app.setLogLevel(Application.LOG_DEBUG);
 
         // TODO: load assets
+        music = Gdx.audio.newMusic(Gdx.files.internal("Sounds/music.mp3"));
+        music.setLooping(true);                // will repeat playback until music.stop() is called
+        music.play();                          // resumes the playback
+
         // TODO: load settings
         batch = new SpriteBatch();
         Gdx.gl.glClearColor(0, 0, 0, 1);
@@ -46,5 +51,12 @@ public class Asteroids extends Game {
 
     public SpriteBatch getBatch() {
         return batch;
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        music.dispose();
+
     }
 }
