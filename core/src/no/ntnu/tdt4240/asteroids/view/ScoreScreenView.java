7f91952e9bec00df6c94bc7b5c537fdedf9a94f7
Sander Aker Christiansen <hanakmumakil23@gmail.com>
2017-04-06 12:21:37 +0200

    ScalingViewport -> FitViewport (#54)

--- core/src/no/ntnu/tdt4240/asteroids/view/ScoreScreenView.java
+++ core/src/no/ntnu/tdt4240/asteroids/view/ScoreScreenView.java
@@ -3,23 +3,23 @@ package no.ntnu.tdt4240.asteroids.view;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.badlogic.gdx.scenes.scene2d.actions.DelayAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.Scaling;
-import com.badlogic.gdx.utils.viewport.ScalingViewport;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 import java.util.List;
 
+import no.ntnu.tdt4240.asteroids.Asteroids;
 import no.ntnu.tdt4240.asteroids.controller.IScoreScreenController;
 import no.ntnu.tdt4240.asteroids.controller.ScoreScreenController;
 
@@ -27,7 +27,8 @@ import no.ntnu.tdt4240.asteroids.controller.ScoreScreenController;
 public class ScoreScreenView extends Stage implements ScoreScreenController.IScoreScreenView {
 
     private static final String TAG = ScoreScreenView.class.getSimpleName();
-    private static Viewport viewport = new ScalingViewport(Scaling.stretch, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+    private static OrthographicCamera camera = new OrthographicCamera();
+    private static Viewport guiViewport = new FitViewport(Asteroids.GUI_VIRTUAL_WIDTH, Asteroids.GUI_VIRTUAL_HEIGHT, camera);
 
     private final Table table = new Table();
 
@@ -39,9 +40,11 @@ public class ScoreScreenView extends Stage implements ScoreScreenController.ISco
     private final IScoreScreenController controller;
 
     public ScoreScreenView(Batch batch, IScoreScreenController controller) {
-        super(viewport, batch);
+        super(guiViewport, batch);
         this.controller = controller;
+        guiViewport.apply(true);
         setDebugAll(true);
+
         table.addAction(Actions.alpha(0));
         addActor(table);
         init();
@@ -53,19 +56,16 @@ public class ScoreScreenView extends Stage implements ScoreScreenController.ISco
         List<String> scores = controller.getScores();
         table.row();
         Label winner = new Label("Last penguin standing: " + scores.get(0).split(" ")[0], defaultLabelStyle);
-        winner.setFontScale(5);
-        table.add(winner).pad(30);
+        table.add(winner).pad(10);
 
         for(int i = 0; i < scores.size(); i++){
             table.row();
-            Label l = new Label(String.valueOf(i+1) + ". Penguin: " + scores.get(i), defaultLabelStyle);
-            l.setFontScale(3);
-            table.add(l).pad(30);
+            Label l = new Label(String.valueOf(i+1) + ". " + scores.get(i), defaultLabelStyle);
+            table.add(l).pad(5);
         }
 
         table.row();
         table.add(menuButton).pad(30);
-        menuButton.getLabel().setFontScale(4);
         menuButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
